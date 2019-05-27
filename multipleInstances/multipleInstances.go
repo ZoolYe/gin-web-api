3b914bd4a3312d9d2775caf94eeb89364803b684
zoolye <www.iphone@foxmail.com>
2019-05-27 11:06:37 +0800

    add instances

---
+++ multipleInstances/multipleInstances.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	g errgroup.Group
+)
+
+func router01() http.Handler {
+	e := gin.New()
+	e.Use(gin.Recovery())
+	e.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "msg": "Welcome server 01"})
+	})
+	return e
+}
+
+func router02() http.Handler {
+	e := gin.New()
+	e.Use(gin.Recovery())
+	e.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "msg": "Welcome server 02"})
+	})
+	return e
+}
+
+func main() {
+
+	server01 := &http.Server{
+		Addr:         ":6240",
+		Handler:      router01(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	server02 := &http.Server{
+		Addr:         ":6241",
+		Handler:      router02(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	g.Go(func() error {
+		return server01.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		return server02.ListenAndServe()
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("------------------------------------------")
+}
